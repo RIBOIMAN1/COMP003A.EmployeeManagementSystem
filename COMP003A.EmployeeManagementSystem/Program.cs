68ea1d43fe0f96fd5aad330301d406606184b31f
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-14 17:42:03 -0800

    Implemented fixed employee credentials (there are currently errors so this is subject to get fixed), added a prompt that asks for the ID of the employee, added a try catch that tells the user that they have inputted an invalid input, and added a public class called "employeeInfo" that has a bunch of get and set commands for the employee's credentials.

--- COMP003A.EmployeeManagementSystem/Program.cs
+++ COMP003A.EmployeeManagementSystem/Program.cs
@@ -5,11 +5,70 @@
 
 namespace COMP003A.EmployeeManagementSystem
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				employeeInfo employee = new employeeInfo();
+				employee.employeeId = "34IG";
+				employee.firstName = "Riley";
+				employee.middleName = "Scott";
+				employee.lastName = "Benson";
+				employee.salary = 75000;
+
+				// Input employee details.
+				Console.Write("Enter Employee ID: ");
+				employee.employeeId = int.Parse(employee.employeeId);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("You have inputted an invalid input, try again.");
+
+			}
+		}
+		/// <summary>
+		/// Showcases employee credentials to the user.
+		/// </summary>
+	}
+	public class employeeInfo
+	{
+		private string _employeeId;
+		private string _firstName;
+		private string _middleName;
+		private string _lastName;
+		private double _salary;
+
+		public string EmployeeId
+		{
+			get { return _employeeId; }
+		}
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+		public string MiddleName
+		{
+			get { return _middleName; }
+			set { _middleName = value; }
+		}
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+		public double Salary
+		{
+			get { return _salary; }
+			set
+			{
+				if (value < 0)
+				{
+					Console.WriteLine("This is an invalid salary value, try again.");
+				}
+			}
+		}
+	}
 }
