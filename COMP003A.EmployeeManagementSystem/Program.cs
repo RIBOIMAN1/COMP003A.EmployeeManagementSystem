d3293124e8c91c6bec614dfb8639f4b86a4cb1e6
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-15 03:03:34 -0800

    Change the public class name from "employeeInfo" to "Employee", added more description to the XML comments for the name inputs karma added an exception comment for the names Cortana and added a better description of why your input could be invalid for your salary.

--- COMP003A.EmployeeManagementSystem/Program.cs
+++ COMP003A.EmployeeManagementSystem/Program.cs
@@ -34,7 +34,7 @@ namespace COMP003A.EmployeeManagementSystem
 	/// <summary>
 	/// Showcases employee credentials to the user.
 	/// </summary>
-	public class employeeInfo
+	public class Employee
 	{
 		private string _employeeId;
 		private string _firstName;
@@ -49,15 +49,23 @@ namespace COMP003A.EmployeeManagementSystem
 			get { return _employeeId; }
 		}
 		/// <summary>
-		/// Gathers the input of the employee's first name.
+		/// Gathers the input of the employee's first name, not allowing empty or null inputs.
 		/// </summary>
+		/// <exception cref="ArgumentException">Exception that gets thrown whenever the input is empty or null.</exception>
 		public string FirstName
 		{
 			get { return _firstName; }
-			set { _firstName = value; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("First name cannot be empty or null.");
+				}
+				_firstName = value;
+			}
 		}
 		/// <summary>
-		/// Gathers the input of the employee's middle name.
+		/// Gathers the input of the employee's middle name, allowing empty but not null inputs.
 		/// </summary>
 		public string MiddleName
 		{
@@ -65,12 +73,20 @@ namespace COMP003A.EmployeeManagementSystem
 			set { _middleName = value; }
 		}
 		/// <summary>
-		/// Gathers the input of the employee's last name.
+		/// Gathers the input of the employee's last name, not allowing empty or null inputs.
 		/// </summary>
+		/// <exception cref="ArgumentException">Exception that gets thrown whenever the input is empty or null.</exception>
 		public string LastName
 		{
 			get { return _lastName; }
-			set { _lastName = value; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("First name cannot be empty or null.");
+				}
+				_lastName = value;
+			}
 		}
 		/// <summary>
 		/// Gathers the input of the employee's salary.
@@ -82,7 +98,7 @@ namespace COMP003A.EmployeeManagementSystem
 			{
 				if (value < 0)
 				{
-					Console.WriteLine("This is an invalid salary value, try again.");
+					Console.WriteLine("This is an invalid salary value since your salary value cannot be less than 0, try again.");
 				}
 			}
 		}
